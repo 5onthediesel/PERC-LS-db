4b09a0e47a5211f8b292fa85c6f54ffac2d49264
5onthediesel <weinshenker.brian@gmail.com>
2026-02-06 14:12:50 -0500

    jdbc statement handling

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # cs370db
+
+cd /Library/PostgreSQL/18/bin
+
+./psql -U postgres -d postgres
+pw rubiks
+
+
+% cd /Users/brianweinshenker/Documents/classes/s26/cs370db
+% javac db.java
+% java -cp /Users/brianweinshenker/Documents/classes/s26/cs370db/postgresql-42.7.8.jar:. db
---
+++ db.java
@@ -0,0 +1,44 @@
+import java.sql.*;
+import java.io.*;
+
+class db {
+
+    public static void main(String args[]) throws IOException
+    {
+        String url;
+        Connection conn;
+        PreparedStatement pStatement;
+        ResultSet rs;
+        String queryString;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println("Failed to find the JDBC driver");
+        }
+
+        try
+        {
+            url = "jdbc:postgresql://localhost:5432/postgres";
+            conn = DriverManager.getConnection(url, "postgres", "rubiks");
+
+            queryString = "drop table if exists cs370";
+            pStatement = conn.prepareStatement(queryString);
+            int result = pStatement.executeUpdate();
+
+            queryString = "create table cs370 (latitude char(40), longitude char(40), url char(40))";
+            pStatement = conn.prepareStatement(queryString);
+            result = pStatement.executeUpdate();
+
+
+        }
+        catch (SQLException se)
+        {
+            System.err.println("SQL Exception." +
+                    "<Message>: " + se.getMessage());
+        }
+
+    }
+
+}
