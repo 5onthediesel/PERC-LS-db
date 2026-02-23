0807c1de2a996ee727567f8cce327ffd1f75b363
Victor <victorli1076@gmail.com>
2026-02-23 12:08:53 -0500

    added service account key

--- database/src/main/java/com/example/db.java
+++ database/src/main/java/com/example/db.java
@@ -24,12 +24,12 @@ class db {
         String url = "jdbc:postgresql://localhost:5432/postgres";
 
         // BRIAN'S POSTGRES USER & PASS
-        String user = "postgres";
-        String pass = "rubiks";
+        // String user = "postgres";
+        // String pass = "rubiks";
 
         // VICTOR'S POSTGRES USER & PASS
-        // String user = "victorli";
-        // String pass = "rubix";
+        String user = "victorli";
+        String pass = "rubix";
 
         // CARSON's POSTGRES USER & PASS
         // String user = "postgres";
@@ -85,18 +85,45 @@ class db {
 
     private static String weatherCodeToString(int code) {
         switch (code) {
-            case 0: return "Clear sky";
-            case 1: case 2: case 3: return "Partly cloudy";
-            case 45: case 48: return "Fog";
-            case 51: case 53: case 55: return "Drizzle";
-            case 61: case 63: case 65: return "Rain";
-            case 66: case 67: return "Freezing rain";
-            case 71: case 73: case 75: return "Snow";
-            case 77: return "Snow grains";
-            case 80: case 81: case 82: return "Rain showers";
-            case 85: case 86: return "Snow showers";
-            case 95: case 96: case 99: return "Thunderstorm";
-            default: return "Unknown";
+            case 0:
+                return "Clear sky";
+            case 1:
+            case 2:
+            case 3:
+                return "Partly cloudy";
+            case 45:
+            case 48:
+                return "Fog";
+            case 51:
+            case 53:
+            case 55:
+                return "Drizzle";
+            case 61:
+            case 63:
+            case 65:
+                return "Rain";
+            case 66:
+            case 67:
+                return "Freezing rain";
+            case 71:
+            case 73:
+            case 75:
+                return "Snow";
+            case 77:
+                return "Snow grains";
+            case 80:
+            case 81:
+            case 82:
+                return "Rain showers";
+            case 85:
+            case 86:
+                return "Snow showers";
+            case 95:
+            case 96:
+            case 99:
+                return "Thunderstorm";
+            default:
+                return "Unknown";
         }
     }
 
